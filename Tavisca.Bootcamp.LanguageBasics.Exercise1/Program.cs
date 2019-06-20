dcdc01689fd5ee03f93ef335bec5968cddb9e771
Ujjwal Bharti <ubharti@tavisca.in>
2019-06-20 12:59:24 +0530

    Add implementation of FindDigit

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,8 +22,49 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            string a = equation.Substring(0, equation.IndexOf('*'));
+            string b = equation.Substring(equation.IndexOf('*') + 1, equation.IndexOf('=') - equation.IndexOf('*') - 1);
+            string c = equation.Substring(equation.IndexOf('=') + 1);
+            if (a.Contains('?'))
+            {
+                int ans = (int.Parse(c)) / int.Parse(b);
+                if (ans * int.Parse(b) == int.Parse(c))
+                {
+                    string res = ans.ToString();
+                    if (res.Length == a.Length)
+                    {
+                        int index = a.IndexOf('?');
+                        return int.Parse(res[index].ToString());
+                    }
+                }
+            }
+            else if (b.Contains('?'))
+            {
+                int ans = (int.Parse(c)) / int.Parse(a);
+                if (ans * int.Parse(a) == int.Parse(c))
+                {
+                    string res = ans.ToString();
+                    if (res.Length == b.Length)
+                    {
+                        int index = b.IndexOf('?');
+                        return int.Parse(res[index].ToString());
+                    }
+                }
+            }
+            else
+            {
+                int ans = int.Parse(a) * int.Parse(b);
+                if (int.Parse(a) * int.Parse(b) == ans)
+                {
+                    string res = ans.ToString();
+                    if (res.Length == c.Length)
+                    {
+                        int index = c.IndexOf('?');
+                        return int.Parse(res[index].ToString());
+                    }
+                }
+            }
+            return -1;
         }
     }
 }
